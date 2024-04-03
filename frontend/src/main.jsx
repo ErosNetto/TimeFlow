3532963a9cc388cd86ccb8d8e3de9b6b6edb38da
Eros Netto Antunes <erosnetto1002@gmail.com>
2024-04-03 16:17:45 -0300

    Service e slice for userAuth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+// Redux
+import { Provider } from "react-redux";
+import { store } from "./store.js";
+
 // React router
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -36,6 +40,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
---
+++ frontend/src/services/authService.js
@@ -0,0 +1,24 @@
+import { api, requestConfig } from "../utils/config";
+
+// Register an user
+const register = async (data) => {
+  const config = requestConfig("POST", data);
+
+  try {
+    const res = await fetch(`${api}/users/register`, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    if (res) {
+      localStorage.setItem("user", JSON.stringify(res));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const authService = {
+  register,
+};
+
+export default authService;
