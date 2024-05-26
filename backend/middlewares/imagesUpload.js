e47180629bd552adb886b0195f76c8a400eb4979
Eros Netto Antunes <erosnetto1002@gmail.com>
2024-05-26 14:55:05 -0300

    Arrumando

--- backend/middlewares/imagesUpload.js
+++ backend/middlewares/imagesUpload.js
@@ -1,8 +1,34 @@
 const multer = require("multer");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const fse = require("fs-extra");
 
-// Generate a random number
-const randomNumber = () => Math.floor(Math.random() * 10000 + 10000);
+// Função para criar os diretórios necessários de forma assíncrona
+const ensureUploadsFolder = async () => {
+  const uploadsPath = path.join(__dirname, "..", "uploads");
+  const adsPath = path.join(uploadsPath, "companies");
+  const depoimentPath = path.join(uploadsPath, "professionals");
+
+  try {
+    // Verifica e cria a pasta uploads
+    await fse.ensureDir(uploadsPath);
+
+    // Verifica e cria a pasta ads
+    await fse.ensureDir(adsPath);
+
+    // Verifica e cria a pasta depoiment
+    await fse.ensureDir(depoimentPath);
+
+    console.log("Pastas criadas com sucesso ou já existem!");
+  } catch (err) {
+    console.error("Erro ao criar pastas:", err);
+  }
+};
+
+// Chama a função para garantir que as pastas existam
+ensureUploadsFolder().catch((err) => {
+  console.error("Erro ao garantir que as pastas existam:", err);
+});
 
 // Destination to store image
 const imagesStorage = multer.diskStorage({
@@ -19,10 +45,7 @@ const imagesStorage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     // Generates a random name for the file and the original file extension
-    cb(
-      null,
-      `${Date.now()}_${randomNumber()}${path.extname(file.originalname)}`
-    );
+    cb(null, `${Date.now()}_${uuidv4()}${path.extname(file.originalname)}`);
   },
 });
 
